1d7e2916c7a8df7635a5f4a0331a69377c8c44fa
auauone <auauone@gmail.com>
2023-07-14 06:17:31 +0700

    add logout process

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,7 +1,11 @@
 <script>
 import { useAppStore } from '../stores/app';
+import ProfileMenuContainer from '../modules/profile/containers/ProfileMenuContainer.vue';
 
 export default {
+  components: {
+    ProfileMenuContainer,
+  },
   setup() {
     const appStore = useAppStore();
     return { appStore };
@@ -18,5 +22,6 @@ export default {
   <v-app-bar prominent>
     <v-app-bar-nav-icon @click="toggleNavbar"></v-app-bar-nav-icon>
     <v-toolbar-title>Application</v-toolbar-title>
+    <ProfileMenuContainer />
   </v-app-bar>
 </template>
